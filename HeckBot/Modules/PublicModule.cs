393eabead68c8a061086307e785e91b568a68495
John Thurmond <grajohnt@yahoo.com>
2019-01-09 19:22:18 -0600

    Modified NerdyGuacamoleBot

--- HeckBot/Modules/PublicModule.cs
+++ HeckBot/Modules/PublicModule.cs
@@ -16,12 +16,20 @@ namespace HeckBot.Modules
         public async Task HelpAsync()
         {
             var msg = "Below is a list of things I can do:\r\n" +
-                "@HeckBot quests - Returns the current and upcoming quests\r\n" +
-                "@HeckBot shield <time> - Starts a shield timer for <time> hours\r\n" +
-                "@HeckBot shield stop - Stops all shield timers\r\n" +
-                "@HeckBot info - Find out what shard you're connected to\r\n" +
-                "@HeckBot cat - purrrr\r\n";
+                "@NerdyGuacamoleBot quests - Returns the current and upcoming quests\r\n" +
+				"@NerdyGuacamoleBot shield <time> - Starts a shield timer for <time> hours\r\n" +
+				"@NerdyGuacamoleBot shield stop - Stops all shield timers\r\n" +
+				"@NerdyGuacamoleBot info - Find out what shard you're connected to\r\n" +
+				"@NerdyGuacamoleBot cat - purrrr\r\n" +
+				"@NerdyGuacamoleBot BattleKing - ???\r\n";
             await ReplyAsync(msg);
         }
+
+		[Command("BattleKing")]
+		public async Task BattleKingAsync()
+		{
+			var msg = $@"Hi {Context.User}! Can YOU teach BattleKing how to use the modulus operator?";
+			await ReplyAsync(msg);
+		}
     }
 }
